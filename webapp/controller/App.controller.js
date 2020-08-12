17c4496c8cf446938a0ce5a58e1d674b45eea75e
lokesh kumar <lokeshkumar24021996@gmail.com>
2020-08-12 07:28:56 +0000

    first change

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("TT.TreeTable.controller.App", {
+		onInit: function () {
+
+		},
+		onSearch:function(oEvent){
+			debugger;
+		}
+	});
+});
